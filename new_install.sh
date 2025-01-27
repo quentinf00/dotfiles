b9a47ed13c5ce3114d33ed6053a8ee3db498bc42
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2025-01-27 13:21:06 +0000

    update install

--- new_install.sh
+++ new_install.sh
@@ -1,16 +1,34 @@
 !#/bin/bash
+
+
+    which "$@" &> /dev/null
+}
+
+
+
 echo "######"
 echo "###### Install 1 \t\t: Installing and activating mise"
 echo "######"
-curl https://mise.run | sh
-eval "$(~/.local/bin/mise activate bash)"
+if is_exe mise; then
+	echo "Mise already installed"
+else:
+	curl https://mise.run | sh
+	eval "$(~/.local/bin/mise activate bash)"
+fi
 
 echo "######"
 echo "###### Install 2 \t\t: Installing pixi"
 echo "######"
-mise use --env local chezmoi
-eval "$(~/.local/bin/mise activate bash)"
-chezmoi init quentinf00 -av
+
+if is_exe chezmoi; then
+	echo "Chezmoi already installed"
+else:
+	mise use --env local chezmoi
+	eval "$(~/.local/bin/mise activate bash)"
+	chezmoi init quentinf00 -v
+fi
+
+chezmoi apply
 
 
 echo "######"
